f5342e29999feae8d72ce251729d09a600df58b2
Webzonedk <webzone@kpdesign.dk>
2022-11-14 12:16:37 +0100

    Projekt is born

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { UploadComponent } from './views/upload/upload.component';
+import { DatagridComponent } from './views/datagrid/datagrid.component';
+import { AddPropertiesComponent } from './modals/add-properties/add-properties.component';
+import { ViewPropertiesComponent } from './modals/view-properties/view-properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UploadComponent,
+    DatagridComponent,
+    AddPropertiesComponent,
+    ViewPropertiesComponent
   ],
   imports: [
     BrowserModule,
